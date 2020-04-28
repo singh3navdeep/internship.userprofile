18bd78cdbfb68c07c34c69ab14a91e81c852bacd
singh3navdeep <navdeep3singh@yahoo.com>
2020-04-28 22:59:05 +0530

    service-discovery disabled

--- src/main/java/in/dailyhunt/internship/userprofile/services/NewsComponentsServiceImpl.java
+++ src/main/java/in/dailyhunt/internship/userprofile/services/NewsComponentsServiceImpl.java
@@ -15,7 +15,7 @@ import in.dailyhunt.internship.userprofile.repositories.LocalityDataRepository;
 import in.dailyhunt.internship.userprofile.repositories.TagDataRepository;
 import in.dailyhunt.internship.userprofile.services.interfaces.NewsComponentsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Service;
 import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.reactive.function.client.WebClient;
