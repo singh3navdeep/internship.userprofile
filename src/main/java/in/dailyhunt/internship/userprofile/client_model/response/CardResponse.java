c083667ea46d42ec5ef4b30ee99d7b72b661b398
singh3navdeep <navdeep3singh@yahoo.com>
2020-05-01 12:17:23 +0530

    CardResponse class updated

--- src/main/java/in/dailyhunt/internship/userprofile/client_model/response/CardResponse.java
+++ src/main/java/in/dailyhunt/internship/userprofile/client_model/response/CardResponse.java
@@ -1,12 +1,16 @@
 package in.dailyhunt.internship.userprofile.client_model.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CardResponse {
     private List<Card> cards;
 }
